81a14038a3f829c8c5755e66323ef9fddc865681
braydenbubp <braydenbubp1@gmail.com>
2023-12-12 21:31:10 -0500

    nav events started

--- components/shared/navBar.js
+++ components/shared/navBar.js
@@ -11,19 +11,16 @@ const navBar = () => {
         <div class="collapse navbar-collapse" id="navbarText">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link" href="#" id="vocab">Vocab</a>
+              <a class="nav-link" href="#" id="view_all">View Orders</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="#" id="Language">Language</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="#" id="Tech">Tech</a>
+            <a class="nav-link" href="#" id="create-order">Create an Order</a>
             </li>
             <li>
             <input
               class="form-control mr-sm-2"
               id="search"
-              placeholder="Search Vocab"
+              placeholder="Search Order"
               aria-label="Search"
             />
             </li>
