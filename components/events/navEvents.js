56704dbc6d3e3457eeb6eb34c9af6166dc5a1522
braydenbubp <braydenbubp1@gmail.com>
2023-12-14 22:09:24 -0500

    fixed data to conform to code

--- components/events/navEvents.js
+++ components/events/navEvents.js
@@ -1,6 +1,8 @@
 import { getOrders } from '../../api/orderData';
+import { showItems } from '../../pages/item';
 import { showOrders } from '../../pages/order';
 import { signOut } from '../../utils/auth';
+import { getItem } from '../../api/itemData';
 
 // navigation events
 const navigationEvents = () => {
@@ -16,16 +18,9 @@ const navigationEvents = () => {
     console.warn('CLICKED CREATE ORDER BUTTON');
   });
 
-  // STRETCH: SEARCH
-  // document.querySelector('#search').addEventListener('keyup', (e) => {
-  //   const searchValue = document.querySelector('#search').value.toLowerCase();
-  //   console.warn(searchValue);
-
-  //   // WHEN THE USER PRESSES ENTER, MAKE THE API CALL AND CLEAR THE INPUT
-  //   if (e.keyCode === 13) {
-  //     document.querySelector('#search').value = '';
-  //   }
-  // });
+  document.querySelector('#order-details-btn').addEventListener('click', () => {
+    getItem().then(showItems);
+  });
 };
 
 export default navigationEvents;
