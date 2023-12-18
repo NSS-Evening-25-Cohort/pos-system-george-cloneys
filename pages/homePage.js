f87546b555a5dac1b775ff444a9d8f80a7f476e6
Maria Sanli <marisanli.93@gmail.com>
2023-12-17 23:15:24 -0500

    fix margins and button sizes

--- pages/homePage.js
+++ pages/homePage.js
@@ -5,11 +5,11 @@ const homePage = () => {
   clearDom();
   const domString = `
   <div class = welcomeContainer>
-  <h2 style="color: white;">Welcome :D!</h2>
+  <h2 style="color: white; margin-bottom: 200px;">Welcome :D!</h2>
   <div style="display: flex; flex-direction: column; align-items: center;">
-  <button type="button" id="view-orders-btn1" class="btn btn-success">View Orders</button>
-  <button type="button" id="create-orders-btn2" class="btn btn-info">Create an Order</button>
-  <button type="button" id="view-revenue-btn3" class="btn btn-warning">View Revenue</button>
+  <button type="button" id="view-orders-btn1" class="btn btn-success" style="width: 250px; margin-bottom: 10px;">View Orders</button>
+  <button type="button" id="create-orders-btn2" class="btn btn-info" style="width: 250px; margin-bottom: 10px;">Create an Order</button>
+  <button type="button" id="view-revenue-btn3" class="btn btn-warning"style="width: 250px; margin-bottom: 10px;">View Revenue</button>
   </div>
 `;
   renderToDOM('#homeContainer', domString);
