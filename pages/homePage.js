4b6ce42139fff3ce52dd80fdfa785d6c9e12963a
Maria Sanli <marisanli.93@gmail.com>
2023-12-17 23:08:13 -0500

    fixed logo size and centered heading and buttons

--- pages/homePage.js
+++ pages/homePage.js
@@ -4,10 +4,13 @@ import clearDom from '../utils/clearDom';
 const homePage = () => {
   clearDom();
   const domString = `
-  <h2>Welcome :D!</h2>
+  <div class = welcomeContainer>
+  <h2 style="color: white;">Welcome :D!</h2>
+  <div style="display: flex; flex-direction: column; align-items: center;">
   <button type="button" id="view-orders-btn1" class="btn btn-success">View Orders</button>
   <button type="button" id="create-orders-btn2" class="btn btn-info">Create an Order</button>
   <button type="button" id="view-revenue-btn3" class="btn btn-warning">View Revenue</button>
+  </div>
 `;
   renderToDOM('#homeContainer', domString);
   // homeContainer subject to change
